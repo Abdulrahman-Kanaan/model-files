4bdb8ea83e62c5232d6e7b7371d6c48a355e2a1a
AbdulrahmanKanaan <abdulrahman.kanaan44@gmail.com>
2020-10-08 13:01:58 +0300

    version 1

--- src/Providers/ModelFilesServiceProvider.php
+++ src/Providers/ModelFilesServiceProvider.php
@@ -8,9 +8,9 @@ class ModelFilesServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->publishes([
-            __DIR__.'/../../config/model-files.php' => config_path('model-files.php')
-        ], 'model-files-config');
+        // $this->publishes([
+        //     __DIR__.'/../../config/model-files.php' => config_path('model-files.php')
+        // ], 'model-files-config');
 
         $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
     }
